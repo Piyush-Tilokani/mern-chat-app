493d5af2b59a2cf6e19e82cbe1495d4e03d8d4f2
Piyush Tilokani <2021.piyush.tilokani@ves.ac.in>
2024-05-28 16:04:06 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -11,6 +12,8 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
@@ -20,11 +23,13 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 
-app.get('/', (req, res)=>{
-	res.send("Hellow World"); 	
+app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
+app.get('*', (req, res)=>{
+	res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
 })
 
 server.listen(PORT,  ()=>{
 	connectToMongodb();
-	console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port ${PORT} \n Visit http://localhost:${PORT}`);
 });
