bfee4d55f5cb45d00b50b41ad6d15493de5f6a18
Piyush Tilokani <2021.piyush.tilokani@ves.ac.in>
2024-05-28 15:44:42 +0530

    Socket io implemented

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,7 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId } from "../socket/socket.js";
+import { io } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
     try {
@@ -29,13 +31,18 @@ export const sendMessage = async (req, res) => {
             conversation.messages.push(newMessage._id);
         }
 
-        //Socket IO functionality
-
         // await conversation.save();
         // await newMessage.save();
         await Promise.all([conversation.save(), newMessage.save()]);
         //More optimised way to save both conversation and message
-
+        
+        //Socket IO functionality
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if(receiverSocketId){
+            //io.to(<socketId>).emit() sends an event to a specific client
+            io.to(receiverSocketId).emit('newMessage', newMessage);
+        }
+        
         res.status(201).json({newMessage});
 
     } catch (error) {
--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -1,7 +1,12 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
 import { useAuthContext } from "./AuthContext";
-export const SocketContext = createContext();
 import io from 'socket.io-client'
+
+export const SocketContext = createContext();
+
+export const useSocketContext = () => {
+    return useContext(SocketContext)
+}
 
 export const SocketContextProvider = ({children}) =>{
     const[socket, setSocket] = useState(null)
@@ -9,16 +14,24 @@ export const SocketContextProvider = ({children}) =>{
     const {authUser} = useAuthContext()
     useEffect(()=>{
         if(authUser){
-            const socket = io('http://localhost:5000');
+            const socket = io('http://localhost:5000', {
+                query: {
+                    userId: authUser._id
+                }
+            });
             setSocket(socket)
-            return () => socket.close() 
+            socket.on('getOnlineUsers', (users) => {
+                setOnlineUsers(users)
+            });
+
+            return () => socket.close()  //Cleanup function
         }else{
             if(socket){
                 socket.close()
                 setSocket(null)
             }
         }
-    },[])
+    },[authUser])
     return(
         <SocketContext.Provider value={{socket, onlineUsers}}>
             {children}
